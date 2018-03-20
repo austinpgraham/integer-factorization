c39a313baabb17c00fab283249567ebbbd0f7fac
Austin Graham <austingraham@Austins-MacBook-Pro.local>
2018-03-19 20:03:07 -0500

    get prime number

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 build:
-	g++ -std=c++11 -o factor source.cpp
+	g++ -std=c++11 -o factor source.cpp -lNTL -lgmp
 
 run:
 	make build
 	./factor
 
 prod-build:
-	g++ -std=c++11 -O3 -o prod-factor source.cpp
+	g++ -std=c++11 -O3 -o prod-factor source.cpp -lNTL -lgmp
 
 prod-run:
 	make prod-build
--- source.cpp
+++ source.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <NTL/ZZ.h>
+#include <math.h>
 
 using namespace std;
+using namespace NTL;
+
+const long PI = 3;
+
+ZZ powerOfTwo(ZZ power) {
+    ZZ acc = ZZ{2};
+    for(int i = 0; i < power; i++) {
+        acc *= 2;
+    }
+    return acc;
+}
+
+ZZ getBigAssPrimeNumber() {
+    ZZ term = powerOfTwo(ZZ{1024}) - powerOfTwo(ZZ{960}) + powerOfTwo(ZZ{64});
+    term *= (powerOfTwo(ZZ{894})*PI + 129093);
+    return term;
+}
 
 int main() {
+    ZZ p = getBigAssPrimeNumber();
+    cout<<p<<endl;
     return 0;
 }
