f4a164d8f04e2c6a73feb520cb538f9bbf44f570
Josh Birdwell <jbirdwell34@gmail.com>
2018-03-26 22:56:13 -0500

    install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+apt-get install xz-utils
+unxz gmp-6.1.2.tar.xz
+tar xf gmp-6.1.2.tar
+cd gmp-6.1.2/
+sudo ./configure && make && make install
+gunzip ntl-10.5.0.tar.gz
+tar xf ntl-10.5.0.tar
+cd ./ntl-10.5.0/src 
+sudo ./configure && make && make install
